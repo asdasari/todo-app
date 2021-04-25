2bc33ebf557f33fed20ad6f3a76765aac8426724
asdasari <as.dasari1988@gmail.com>
2021-04-25 20:48:54 +0530

    todo app initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo-app';
+  tasks = [];
+  newTask;
+
+  addToList() {
+    this.tasks.push(this.newTask);
+    this.newTask = '';
+  }
+
+  deleteTask(index) {
+    this.tasks.splice(index, 1);
+  }
 }
